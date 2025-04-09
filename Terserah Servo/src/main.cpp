9b966322f32c71309f3746d53d5e4773719451f4
CzperTama <abhirama.pratama@mhs.unsoed.ac.id>
2025-04-09 23:05:50 +0700

    Projek Servo Berputar-putar

--- Terserah Servo/src/main.cpp
+++ Terserah Servo/src/main.cpp
@@ -1,13 +1,17 @@
 #include <Arduino.h>
 #include <Servo.h>
 
+// deklarasi objek servo
 Servo myservo;
 
 void setup() {
+  // fungsi untuk menghubungkan servo pada pin arduino.
   myservo.attach(D7);
 }
 
+// perulangan yang digunakan untuk menggerakan servo
 void loop() {
+  // fungsi yang menggerakan servo dari posisi 0 derajat ke 180 derajat dan kembali ke 0 derajat secara bertahap.
   // for(int pos = 0; pos<=180; pos++){
   //   myservo.write(pos);
   //   delay(10);
@@ -17,6 +21,7 @@ void loop() {
   //   delay(10);
   // }
 
+  // fungsi yang menggerakan servo dari posisi 180 derajat ke 0 derajat dan kembali ke 180 derajat, dengan jeda 2 detik pada setiap posisi.
   myservo.write(180);
   delay(2000);
   myservo.write(0);
